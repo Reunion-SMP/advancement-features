6dd8779f526d871f2a1b3700cc8197f2702214e2
Goinging <146245761+Goinging@users.noreply.github.com>
2025-04-28 19:29:54 -0700

    Adding the dragon-egg advancement feature

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # has-visited-end-context
-LuckPerms context to check for the `minecraft:end/root` advancement.
+LuckPerms context to check for the `minecraft:end/root` advancement
 
 This plugin was made as a one-off for a friend. **Issues and pull requests submitted to this repository will be ignored**.
 
 This was also largely based upon [LuckPerms/ExtraContexts](https://github.com/LuckPerms/ExtraContexts). Thank you for your work.
-
-If you're looking to replicate this in a different way, you can use LuckPerms [LuckPerms/ExtraContexts](https://github.com/LuckPerms/ExtraContexts) and [PAPI](https://www.spigotmc.org/resources/placeholderapi.6245/). Then, just check to see if the `%Advancements_end/root%` placeholder is true.
-
-You can download the latest .jar file of this repo [from this link](https://nightly.link/Avenred/has-visited-end-context/workflows/maven/main/build-artifacts.zip).
--- src/main/java/org/agsfefd/endContext/EndContext.java
+++ src/main/java/org/agsfefd/endContext/EndContext.java
@@ -4,14 +4,21 @@ import org.agsfefd.endContext.calculators.HasVisitedEndCalculator;
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.context.ContextCalculator;
 import net.luckperms.api.context.ContextManager;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
+import org.bukkit.advancement.Advancement;
+import org.bukkit.advancement.AdvancementProgress;
 import org.bukkit.entity.Player;
-
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.function.Supplier;
 
-public final class EndContext extends JavaPlugin {
+public final class EndContext extends JavaPlugin implements Listener {
     private final List<ContextCalculator<Player>> registeredCalculators = new ArrayList<>();
     private ContextManager contextManager;
 
@@ -25,7 +32,10 @@ public final class EndContext extends JavaPlugin {
         getLogger().info("Registering custom context calculators...");
         this.contextManager = luckPerms.getContextManager();
         setup();
+        getServer().getPluginManager().registerEvents(this, this);
+
     }
+    
 
     @Override
     public void onDisable() {
@@ -45,5 +55,45 @@ public final class EndContext extends JavaPlugin {
     private void unregisterAll() {
         this.registeredCalculators.forEach(c -> this.contextManager.unregisterCalculator(c));
         this.registeredCalculators.clear();
+    }
+
+
+    // === Advancement Listeners ===
+
+    @EventHandler
+    public void onAdvancementDone(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        Advancement advancement = event.getAdvancement();
+
+        // Check if player just completed "Free the End"
+        if (advancement.getKey().equals(NamespacedKey.minecraft("end/kill_dragon"))) {
+            giveDragonEggAdvancement(player);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        Advancement freeTheEnd = Bukkit.getAdvancement(NamespacedKey.minecraft("end/kill_dragon"));
+        Advancement dragonEgg = Bukkit.getAdvancement(NamespacedKey.minecraft("end/dragon_egg"));
+
+        if (freeTheEnd != null && dragonEgg != null) {
+            if (hasAdvancement(player, freeTheEnd) && !hasAdvancement(player, dragonEgg)) {
+                giveDragonEggAdvancement(player);
+            }
+        }
+    }
+
+    private boolean hasAdvancement(Player player, Advancement advancement) {
+        AdvancementProgress progress = player.getAdvancementProgress(advancement);
+        return progress.isDone();
+    }
+
+    private void giveDragonEggAdvancement(Player player) {
+        Advancement advancement = Bukkit.getAdvancement(NamespacedKey.minecraft("end/dragon_egg"));
+        AdvancementProgress progress = player.getAdvancementProgress(advancement);
+        for (String criteria : progress.getRemainingCriteria()) {
+            progress.awardCriteria(criteria);
+        }
     }
 }
